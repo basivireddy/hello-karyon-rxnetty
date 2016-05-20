6fc6a028c9304d6718fd3eb4d11b95bf000e4a66
Basivireddy <basivireddy@users.noreply.github.com>
2016-05-20 19:44:28 +0530

    Update KaryonRxNettyExample.java

--- src/main/java/com/kenzan/karyon/rxnetty/KaryonRxNettyExample.java
+++ src/main/java/com/kenzan/karyon/rxnetty/KaryonRxNettyExample.java
@@ -26,7 +26,6 @@ public class KaryonRxNettyExample {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("hi");
-        System.out.println("hi");
         Karyon.forRequestHandler(8080,
                 new ExampleRouteInterceptor(),
                 new ArchaiusBootstrapModule("hello-karyon-rxnetty"),
