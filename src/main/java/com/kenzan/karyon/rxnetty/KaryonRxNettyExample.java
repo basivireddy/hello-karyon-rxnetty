e29dd8b50124ce5cc09d69d1d6a2e7643ee17b78
madhu <madhukarr@ipsgsystems.com>
2016-06-03 10:58:12 +0530

    Update KaryonRxNettyExample.java

--- src/main/java/com/kenzan/karyon/rxnetty/KaryonRxNettyExample.java
+++ src/main/java/com/kenzan/karyon/rxnetty/KaryonRxNettyExample.java
@@ -23,7 +23,7 @@ import netflix.karyon.archaius.ArchaiusBootstrapModule;
 public class KaryonRxNettyExample {
 
     public static void main(String[] args) {
-        System.out.println("hi kkkk");
+        System.out.println("hi madhu");
         System.out.println("hi");
         System.out.println("hi");
         Karyon.forRequestHandler(8080,
