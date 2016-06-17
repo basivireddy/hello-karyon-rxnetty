5e935feb40395184ed99b8267b420d34b052aa9b
kiran <crackerplace@gmail.com>
2016-06-17 19:24:21 +0530

    Update HelloEndpoint.java

--- src/main/java/com/kenzan/karyon/rxnetty/endpoint/HelloEndpoint.java
+++ src/main/java/com/kenzan/karyon/rxnetty/endpoint/HelloEndpoint.java
@@ -19,7 +19,7 @@ import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.protocol.http.server.HttpServerRequest;
 
 import java.util.regex.Pattern;
-
+-
 import com.sun.jersey.api.uri.UriPattern;
 
 import rx.Observable;
@@ -27,13 +27,13 @@ import rx.Observable;
 public class HelloEndpoint {
 
     public Observable<String> getHello() {
-        return Observable.just("Hello ,how are you");
+        return Observable.just("Hello ,how are you 1.1");
     }
 
     public Observable<String> getHelloName(HttpServerRequest<ByteBuf> request) {
         UriPattern pattern = new UriPattern(Pattern.compile("/hello/(.*)"));
         String name = pattern.match(request.getUri()).group(1);
 
-        return Observable.just("Hello,how are you " + name);
+        return Observable.just("Hello,how are you 1.1 " + name);
     }
 }
