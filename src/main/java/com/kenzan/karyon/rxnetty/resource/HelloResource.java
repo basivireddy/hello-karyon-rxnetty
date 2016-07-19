d428ee48759dabc97a1a309ec2fc088298a82311
kiran <crackerplace@gmail.com>
2016-07-19 18:32:06 +0530

    Update HelloResource.java

--- src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
+++ src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
@@ -34,7 +34,6 @@ public class HelloResource implements RequestHandler<ByteBuf, ByteBuf>{
         endpoint = new HelloEndpoint();
         delegate = new SimpleUriRouter<>();
 
-
         delegate
         .addUri("/hello", new RequestHandler<ByteBuf, ByteBuf>() {
             @Override
