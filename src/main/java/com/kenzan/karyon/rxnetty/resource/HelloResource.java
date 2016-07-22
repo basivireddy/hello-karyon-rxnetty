305461090dcbf6e44b25db3df701bf8979701f4f
kiran <crackerplace@gmail.com>
2016-07-22 21:09:28 +0530

    Update HelloResource.java

--- src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
+++ src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
@@ -70,12 +70,12 @@ public class HelloResource implements RequestHandler<ByteBuf, ByteBuf>{
     public Observable<Void> handle(HttpServerRequest<ByteBuf> request,
             HttpServerResponse<ByteBuf> response) {
 
-              /*  try {
+               try {
                     Thread.sleep(10);
                 }
                 catch(Exception exec){
                     System.out.println("Interrupted exception");
-                } */
+                } 
 
         return delegate.handle(request, response);
     }
