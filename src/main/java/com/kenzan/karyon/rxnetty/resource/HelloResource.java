e104cdf95e3e046b71acec9cb9e374adf20eeeaa
kiran <crackerplace@gmail.com>
2016-08-10 19:12:03 +0530

    Update HelloResource.java

--- src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
+++ src/main/java/com/kenzan/karyon/rxnetty/resource/HelloResource.java
@@ -72,7 +72,7 @@ public class HelloResource implements RequestHandler<ByteBuf, ByteBuf>{
     @Override
     public Observable<Void> handle(HttpServerRequest<ByteBuf> request,
             HttpServerResponse<ByteBuf> response) {
-               for(int i =0;i<2;i++){
+               /*for(int i =0;i<2;i++){
                    leakMap.put(new BadKey("key"), "value");
                }    
                try {
@@ -80,7 +80,7 @@ public class HelloResource implements RequestHandler<ByteBuf, ByteBuf>{
                 }
                 catch(Exception exec){
                     System.out.println("Interrupted exception");
-                }
+                }*/
             
 
         return delegate.handle(request, response);
